3b0bab245c293bbee81dc9c727702fa337c1184c
VitorHUMoreira <vitorhugo_187@hotmail.com>
2022-09-30 12:37:43 -0300

    add production api

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:4000",
-  production: "LINK DO SERVIDOR DEPLYADO AQUI !!!!",
+  production: "https://lab-recipe.herokuapp.com",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
